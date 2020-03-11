305ef43b4b138660bdfd1cdee638cce47e487769
Ömer Demir <omer.demir-@hotmail.com>
2020-03-11 04:14:39 +0300

    first commit

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChromeRecovery
+{
+    public class Account
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string URL { get; set; }
+
+        public string Application { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChromeRecovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Chromium.Grab();
+
+            foreach (var b in a)
+            {
+                Console.WriteLine("Url: " + b.URL);
+                Console.WriteLine("Username: " + b.UserName);
+                Console.WriteLine("Password: " + b.Password);
+                Console.WriteLine("Application: " + b.Application);
+                Console.WriteLine("=============================");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
